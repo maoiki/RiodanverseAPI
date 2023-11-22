1052b6fe37599abb57bfced3d82b7e105b0f1fb0
Julio Duarte <73315417+maoiki@users.noreply.github.com>
2023-11-21 22:34:02 -0300

    #3 fix: ajusta entidade acampamento

--- riodanverse/src/main/java/riordanverse/riordanverse/entities/Acampamento.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/entities/Acampamento.java
@@ -1,10 +1,15 @@
 package riordanverse.riordanverse.entities;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter @Setter
 public class Acampamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
