7bd3f7a22dc159d949c506010bd8fdade62e5bd9
Julio Duarte <73315417+maoiki@users.noreply.github.com>
2023-11-22 15:47:53 -0300

    #9 feat: adiciona busca por nomes e buscar todos

--- riodanverse/src/main/java/riordanverse/riordanverse/controllers/MitologiaController.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/controllers/MitologiaController.java
@@ -1,5 +1,7 @@
 package riordanverse.riordanverse.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,5 +23,17 @@ public class MitologiaController {
      public Mitologia getMitologia(@PathVariable Integer idMitologia){
           Mitologia mitologia = mitologiaService.getMitologia(idMitologia);
           return mitologia;
+     }
+
+     @GetMapping("/nome/{nomeMitologia}")
+     public Mitologia getMitologiaByNome(@PathVariable String nomeMitologia){
+          Mitologia mitologia = mitologiaService.getMitologiaByNome(nomeMitologia);
+          return mitologia;
+     }
+
+     @GetMapping("/all")
+     public List<Mitologia> getAllMitologias(){
+          List<Mitologia> mitologias = mitologiaService.getAllMitologias();
+          return mitologias;
      }
 }
