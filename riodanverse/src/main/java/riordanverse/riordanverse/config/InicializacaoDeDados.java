178af9ac4439e1ef9b75c06569a34fca3bd2b77c
Julio Duarte <julio.franca.017@ufrn.edu.br>
2023-11-22 22:20:27 -0300

    #10 feat: adiciona criação inicial de livros

--- riodanverse/src/main/java/riordanverse/riordanverse/config/InicializacaoDeDados.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/config/InicializacaoDeDados.java
@@ -5,17 +5,20 @@ import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
 import riordanverse.riordanverse.services.AcampamentoService;
+import riordanverse.riordanverse.services.LivroService;
 import riordanverse.riordanverse.services.MitologiaService;
 
 @Component
 public class InicializacaoDeDados {
     private final MitologiaService mitologiaService;
     private final AcampamentoService acampamentoService;
+    private final LivroService livroService;
 
     @Autowired
-    public InicializacaoDeDados(MitologiaService mitologiaService, AcampamentoService acampamentoService) {
+    public InicializacaoDeDados(MitologiaService mitologiaService, AcampamentoService acampamentoService, LivroService livroService) {
         this.mitologiaService = mitologiaService;
         this.acampamentoService = acampamentoService;
+        this.livroService = livroService;
     }
 
     
@@ -24,5 +27,6 @@ public class InicializacaoDeDados {
     public void inicializarDados() {
         mitologiaService.criarMitologiasIniciais();
         acampamentoService.criarAcampamentosIniciais();
+        livroService.criarLivrosIniciais();
     }
 }
--- riodanverse/src/main/java/riordanverse/riordanverse/repositories/LivroRepository.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/repositories/LivroRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import riordanverse.riordanverse.entities.Livro;
 
 public interface LivroRepository extends JpaRepository<Livro, Integer>{
-     
+    Livro findByNome(String nome);
 }
