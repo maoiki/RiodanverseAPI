e5a000a7cd48771fc90b10b9e85f57763648201d
Julio Duarte <julio.franca.017@ufrn.edu.br>
2023-11-30 13:16:37 -0300

    #10 feat: cria base de service de Usuario

---
+++ riodanverse/src/main/java/riordanverse/riordanverse/services/UsuarioService.java
@@ -0,0 +1,33 @@
+package riordanverse.riordanverse.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import riordanverse.riordanverse.entities.Usuario;
+import riordanverse.riordanverse.repositories.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> getAllUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario salvar (Usuario usuario){
+          return usuarioRepository.save(usuario);
+     }
+
+     public Usuario atualizar(Usuario usuario){
+          return usuarioRepository.save(usuario);
+     }
+
+     public void remover(Integer idUsuario){
+          usuarioRepository.deleteById(idUsuario);
+     }
+    
+}
