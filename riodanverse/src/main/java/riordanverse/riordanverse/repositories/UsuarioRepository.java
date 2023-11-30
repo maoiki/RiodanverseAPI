a5646d7fffcb1a9b1320e91366b8fb09af54b36e
Julio Duarte <julio.franca.017@ufrn.edu.br>
2023-11-30 13:15:18 -0300

    #11 feat: adiciona buscas no repository de Usuario

--- riodanverse/src/main/java/riordanverse/riordanverse/repositories/UsuarioRepository.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/repositories/UsuarioRepository.java
@@ -1,9 +1,16 @@
 package riordanverse.riordanverse.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import riordanverse.riordanverse.entities.Acampamento;
+import riordanverse.riordanverse.entities.Criatura;
 import riordanverse.riordanverse.entities.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    
+    Usuario findByNome(String nome);
+    List<Usuario> findByFuncao(String funcao);
+    List<Usuario> findByCriatura(Criatura criatura);
+    List<Usuario> findByAcampamento(Acampamento acampamento);
 }
