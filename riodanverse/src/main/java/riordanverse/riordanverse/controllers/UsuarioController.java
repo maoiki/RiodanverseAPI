7a331c147f39a6a9dcd99c7094a55bc0ad18f4e3
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2023-12-04 10:39:03 -0300

    #12 feat: cria metodos no controller e service do usuario

--- riodanverse/src/main/java/riordanverse/riordanverse/controllers/UsuarioController.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/controllers/UsuarioController.java
@@ -1,14 +1,99 @@
 package riordanverse.riordanverse.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import riordanverse.riordanverse.entities.Usuario;
+import riordanverse.riordanverse.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
-    @GetMapping
-    public String teste(){
-        return "teste";
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping("/all")
+    public List<Usuario> getAllUsuarios(){
+        List <Usuario> usuarios = usuarioService.getAllUsuarios();
+        return usuarios;
+    }
+
+    @GetMapping("/login/{nomeLogin}")
+    public Usuario getUsuarioByLogin(@PathVariable String nomeLogin){
+        Usuario usuario = usuarioService.getUsuarioByLogin(nomeLogin);
+        return usuario;
+    }
+
+    @GetMapping("/criatura/{nomeCriatura}")
+    public List<Usuario> getUsuarioByCriatura(@PathVariable String nomeCriatura){
+        List<Usuario> usuarios = usuarioService.getUsuarioByCriatura(nomeCriatura);
+
+        if(usuarios.isEmpty()){
+            throw new IllegalStateException("Não foram encontrados resultados para essa busca!");
+        }
+
+        return usuarios;
+    }
+
+    @GetMapping("/acamp/{acampNome}")
+    public List<Usuario> getUsuarioByAcampamento(@PathVariable String acampNome){
+        List<Usuario> usuarios = usuarioService.getUsuarioByAcampamento(acampNome);
+        
+        if(usuarios.isEmpty()){
+            throw new IllegalStateException("Não foram encontrados resultados para essa busca!");
+        }
+
+        return usuarios;
+    }
+
+
+    @PostMapping
+    public String cadastrarUsuario(@RequestBody Usuario usuario){
+        String login = usuario.getLogin();
+        Usuario existente = usuarioService.getUsuarioByLogin(login);
+
+        if(existente != null){
+            throw new IllegalStateException("Já existe um usuario com o login: " + login); 
+        }
+
+        usuarioService.salvar(usuario);
+
+        String feedback = "Usuario "+login+" cadastrado com sucesso!";
+        return feedback;
+
+    }
+
+    @PutMapping
+    public String atualizarUsuario(@RequestBody Usuario usuario){
+        String login = usuario.getLogin();
+        Usuario existente = usuarioService.getUsuarioByLogin(login);
+
+        if(existente != null){
+            throw new IllegalStateException("Já existe um usuario com o login: " + login); 
+        }
+
+         usuarioService.salvar(usuario);
+
+        String feedback = "Usuario "+login+" cadastrado com sucesso!";
+        return feedback;
+    }
+
+    @DeleteMapping("/id/{idUser}")
+    public String removerUser(@PathVariable Integer idUser){
+        Usuario existente = usuarioService.getUsuarioById(idUser);
+        
+        usuarioService.remover(idUser);
+        String feedback = "Usuario " + existente.getLogin() +" removido com sucesso!";
+          return feedback;
     }
 }
--- riodanverse/src/main/java/riordanverse/riordanverse/services/UsuarioService.java
+++ riodanverse/src/main/java/riordanverse/riordanverse/services/UsuarioService.java
@@ -1,10 +1,13 @@
 package riordanverse.riordanverse.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import riordanverse.riordanverse.entities.Acampamento;
+import riordanverse.riordanverse.entities.Criatura;
 import riordanverse.riordanverse.entities.Usuario;
 import riordanverse.riordanverse.repositories.UsuarioRepository;
 
@@ -14,12 +17,34 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private CriaturaService criaturaService;
+
+    @Autowired
+    private AcampamentoService acampamentoService;
+
+
     public Usuario getUsuarioByLogin(String login){
      return usuarioRepository.findByLogin(login);
     }
 
+    public Usuario getUsuarioById(Integer idUser){
+     Optional<Usuario> usuario = usuarioRepository.findById(idUser);
+     return usuario.get();
+    }
+
     public List<Usuario> getAllUsuarios(){
         return usuarioRepository.findAll();
+    }
+
+     public List<Usuario> getUsuarioByCriatura(String criaturaNome){
+          Criatura criatura = criaturaService.getCriaturaByNome(criaturaNome);
+          return usuarioRepository.findByCriatura(criatura);
+    }
+
+    public List<Usuario> getUsuarioByAcampamento(String acampNome){
+          Acampamento acamp = acampamentoService.getAcampamentoByNome(acampNome);
+          return usuarioRepository.findByAcampamento(acamp);
     }
 
     public Usuario salvar (Usuario usuario){
